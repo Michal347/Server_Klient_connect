6c0448999938fae643ab40643d7d3d446864e0f1
Michal347 <mczul483@gmail.com>
2021-09-03 13:39:26 +0200

    Add project files.

---
+++ Klient/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+
+public class clnt
+{
+
+    public static void Main()
+    {
+
+        try
+        {
+            TcpClient tcpclnt = new TcpClient();
+            Console.WriteLine("łączenie .....");
+
+            tcpclnt.Connect("192.168.1.107", 8001);
+            
+
+            Console.WriteLine("Połączono ");
+            Console.Write("Wprowadź ciąg do przesłania : ");
+
+            String str = Console.ReadLine();
+            Stream stm = tcpclnt.GetStream();
+
+            ASCIIEncoding asen = new ASCIIEncoding();
+            byte[] ba = asen.GetBytes(str);
+            Console.WriteLine("Transmitowanie.....");
+
+            stm.Write(ba, 0, ba.Length);
+
+            byte[] bb = new byte[100];
+            int k = stm.Read(bb, 0, 100);
+
+            for (int i = 0; i < k; i++)
+                Console.Write(Convert.ToChar(bb[i]));
+
+            tcpclnt.Close();
+            Console.ReadKey();
+        }
+
+        catch (Exception e)
+        {
+            Console.WriteLine("Błąd..... " + e.StackTrace);
+        }
+    }
+
+}
---
+++ Serwer/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+public class serv
+{
+    public static void Main()
+    {
+        try
+        {
+            IPAddress ipAd = IPAddress.Parse("192.168.1.107");
+         
+            TcpListener myList = new TcpListener(ipAd, 8001);
+
+      
+            myList.Start();
+
+            Console.WriteLine("Serwer działa na porcie 8001...");
+            Console.WriteLine("Lokalny punkt końcowy to :" +
+                              myList.LocalEndpoint);
+            Console.WriteLine("Oczekiwanie na połączenie.....");
+
+            Socket s = myList.AcceptSocket();
+            Console.WriteLine("Połączenie zaakceptowane od " + s.RemoteEndPoint);
+
+            byte[] b = new byte[100];
+            int k = s.Receive(b);
+            Console.WriteLine("Otrzymano...");
+            for (int i = 0; i < k; i++)
+                Console.Write(Convert.ToChar(b[i]));
+
+            ASCIIEncoding asen = new ASCIIEncoding();
+            s.Send(asen.GetBytes("Ciąg został odebrany przez serwer."));
+            Console.WriteLine("\nWysłane potwierdzenie");
+           
+            s.Close();
+            myList.Stop();
+            Console.ReadKey();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Błąd..... " + e.StackTrace);
+        }
+    }
+
+}
